d3a6e4669787efd90ac64b90733cd3f0ca49f14c
Sprawl-bebop <95238409+Sprawl-bebop@users.noreply.github.com>
2022-08-11 17:50:18 +0545

    Add files via upload

---
+++ Javascript/course-bbm.js
@@ -0,0 +1,35 @@
+
+/* Show Hide Overview or Curriculum */
+$(document).ready(function(){
+    $("#overview-btn").click(function(){
+      $(".curriculum").hide();
+      $(".overview").show();
+    });
+
+    $("#curricum-btn").click(function(){
+      $(".overview").hide();
+      $(".curriculum").show();
+    });
+  });
+
+
+
+  /* Semester toogle*/ // .curriculum .active
+  $(function() {
+    $(".curriculum button").on('click',function(){
+      $(".curriculum>div .panel.active").removeClass("active");
+      $(this).addClass("active");
+      var panelId = $(this).attr("data-panel");
+      // alert(panelId);
+
+      $(".curriculum .panel.active").slideToggle(300, function(){
+        $(this).removeClass("active");
+
+        $("#"+panelId).slideToggle(300, function(){
+          $(this).addClass("active");
+        });
+      });
+
+      
+    });
+});
